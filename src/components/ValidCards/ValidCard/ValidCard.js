2f49a055d8cbfeceb4580b1ba3e1627a6702e60b
filippoimarisio <filippo.imarisio@gmail.com>
2019-04-24 23:19:48 +0200

    final styling

--- src/components/ValidCards/ValidCard/ValidCard.js
+++ src/components/ValidCards/ValidCard/ValidCard.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './ValidCard.css';
 
-  const ValidCard = props => {
+const ValidCard = props => {
   
   const numberLength = props.card.number.length
-  const cardNumber = `xxxx xxxx xxxx xxxx ${props.card.number[numberLength - 1]}${props.card.number[numberLength - 2]}${props.card.number[numberLength - 3]}`
+  const cardNumber = `**** **** **** **** ${props.card.number[numberLength - 1]}${props.card.number[numberLength - 2]}${props.card.number[numberLength - 3]}`
   const discount = props.card.discount
 
   return (
     <li className="valid-card">
-      <span className="valid-card__label">Gift Card</span>
-      <div className="valid-card__details">
-        <span className="valid-card__number">{cardNumber}</span>
-        <span className="valid-card__discount">{discount}</span>
+      <div className="valid-card__left-column">
+        <div className="valid-card__label">Gift Card</div>
+        <div className="valid-card__number">{cardNumber}</div>
+      </div>
+      <div className="valid-card__right-column">
+        <div className="valid-card__discount">{discount}</div>
       </div>
     </li>
   );
